1b9f41e05b27a9c3a32aa7955713d47a1f8d8d4b
ksdyb.park <ksdyb@amazon.com>
2023-02-04 17:27:01 +0900

    update

--- image2image/stable-diffusion-image2image.py
+++ image2image/stable-diffusion-image2image.py
@@ -2,34 +2,79 @@ import requests
 import torch
 from PIL import Image
 from io import BytesIO
+from diffusers import StableDiffusionImg2ImgPipeline
 
-from diffusers import StableDiffusionPipeline
+from cv2 import imread
+from numpy import load
+import os
+from logging import INFO, StreamHandler, getLogger
+logger = getLogger()
+logger.setLevel(INFO)
+
+def load_image(image_path):
+    # Case insenstive check of the image type.
+    img_lower = image_path.lower()
+    if (
+        img_lower.endswith(
+            ".jpg",
+            -4,
+        )
+        or img_lower.endswith(
+            ".png",
+            -4,
+        )
+        or img_lower.endswith(
+            ".jpeg",
+            -5,
+        )
+    ):
+        try:
+            image_data = imread(image_path)
+        except Exception as e:
+            logger.error(
+                "Unable to read the image at: {}. Error: {}".format(image_path, e)
+            )
+            exit(1)
+    elif img_lower.endswith(
+        ".npy",
+        -4,
+    ):
+        image_data = load(image_path)
+    else:
+        logger.error("Images of format jpg,jpeg,png and npy are only supported.")
+        exit(1)
+    return image_data
 
 def imageToimage(device):
     model_id = "runwayml/stable-diffusion-v1-5"
 
+    # load the pipeline
     if device == "cuda":
-        pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+        pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     else:
-        pipe = StableDiffusionPipeline.from_pretrained(model_id)    
+        pipe = StableDiffusionImg2ImgPipeline.from_pretrained(model_id)    
 
-    # or download via git clone https://huggingface.co/runwayml/stable-diffusion-v1-5
-    # and pass `model_id="./stable-diffusion-v1-5"`.
     pipe = pipe.to(device)
 
-    # let's download an initial image
-    url = "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/assets/stable-samples/img2img/sketch-mountains-input.jpg"
+    # 
+    #url = "https://raw.githubusercontent.com/CompVis/stable-diffusion/main/assets/stable-samples/img2img/sketch-mountains-input.jpg"
+    #prompt = "A fantasy landscape, trending on artstation"
 
-    response = requests.get(url)
-    init_image = Image.open(BytesIO(response.content)).convert("RGB")
-    init_image = init_image.resize((768, 512))
+    #response = requests.get(url)
+    #init_image = Image.open(BytesIO(response.content)).convert("RGB")
+    #init_image = init_image.resize((768, 512))
 
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
+    print('BASE_DIR = ', BASE_DIR)
+    fname = "pelican.jpeg"
     prompt = "A fantasy landscape, trending on artstation"
+
+    init_image = load_image(os.path.join(BASE_DIR, fname))
+
 
     images = pipe(prompt=prompt, image=init_image, strength=0.75, guidance_scale=7.5).images
 
     images[0].save("fantasy_landscape.png")
-
 
 def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
--- text2image/stable-diffusion-text2image.py
+++ text2image/stable-diffusion-text2image.py
@@ -1,11 +1,10 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
-def main():
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    print("device: "+ device)
-
-    model_id = "runwayml/stable-diffusion-v1-5"
+def text2image(device):
+    # model_id = "runwayml/stable-diffusion-v1-5"
+    model_id = "../stable-diffusion-v1-5"
+    
     if device == "cuda":
         pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     else:
@@ -17,6 +16,12 @@ def main():
     image = pipe(prompt).images[0]  
         
     image.save("astronaut_rides_horse.png")
+
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("device: "+ device)
+
+    text2image(device)
 
 if __name__ == '__main__':
     main()
