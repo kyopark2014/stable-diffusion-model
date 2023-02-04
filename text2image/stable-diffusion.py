c14034893517bab51f610eb97c789e3d1ab5805a
ksdyb.park <ksdyb@amazon.com>
2023-02-04 13:41:05 +0900

    update

--- text2image/stable-diffusion.py
+++ text2image/stable-diffusion.py
@@ -1,11 +1,18 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
-pipe = pipe.to("cuda")
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("device: "+ device)
 
-prompt = "a photo of an astronaut riding a horse on mars"
-image = pipe(prompt).images[0]  
-    
-image.save("astronaut_rides_horse.png")
+    model_id = "runwayml/stable-diffusion-v1-5"
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+    pipe = pipe.to(device)
+
+    prompt = "a photo of an astronaut riding a horse on mars"
+    image = pipe(prompt).images[0]  
+        
+    image.save("astronaut_rides_horse.png")
+
+if __name__ == '__main__':
+    main()
